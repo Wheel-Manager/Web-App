8355d6ce2ff48c66f317a17c2ac22ec5b5aabd1b
Hugo Arauco <araparhug7@gmail.com>
2021-06-12 10:20:57 -0500

    Create feature rent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RentComponent} from './pages/rent/rent.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/model/vehicle.ts
@@ -0,0 +1,9 @@
+export class Vehicle {
+  id: number;
+  typeName: string;
+
+  constructor() {
+    this.id = 0;
+    this.typeName = '';
+  }
+}
